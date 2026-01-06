cdc61e58d537b047cc54fd50141cb7338816cc39
Javon <jhyche80@gmail.com>
2026-01-06 13:20:01 -0500

    Changed some more files

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import DataProvider from './Provider/DataProvider.tsx'
 import React from 'react'
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </HashRouter>
+    </BrowserRouter>
 
   </React.StrictMode>,
 )
