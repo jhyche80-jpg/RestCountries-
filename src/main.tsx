1c9ce0eeef0cbd711a6004ab7a1b92d1f926df25
Javon <jhyche80@gmail.com>
2026-01-06 12:09:11 -0500

    I made changes to see if the code will deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import DataProvider from './Provider/DataProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>,
 )
